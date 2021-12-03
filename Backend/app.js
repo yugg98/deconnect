24e46745fbbffa7b562b39651e7005e3ae28eaed
yugg98 <70101980+yugg98@users.noreply.github.com>
2021-12-03 16:38:15 +0530

    server

--- Backend/app.js
+++ Backend/app.js
@@ -25,6 +25,6 @@ app.use(express.static(path.join(__dirname, "./build")));
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../Frontend/build/index.html"));
 });
 module.exports = app;
