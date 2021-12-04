722258ff04909bb3389f8aa63c9a33a102577f21
yugg98 <70101980+yugg98@users.noreply.github.com>
2021-12-04 12:34:51 +0530

    yug

--- Frontend/src/components/TechShorts/NewsCard.js
+++ Frontend/src/components/TechShorts/NewsCard.js
@@ -8,8 +8,8 @@ const NewsCard = ({ newsItem }) => {
             <img
                 alt={newsItem.title}
                 src={
-                    newsItem.img
-                        ? newsItem.img
+                    newsItem.url
+                        ? newsItem.url
                         : "http://www.aaru.edu.jo/websites/aaru2/wp-content/plugins/learnpress/assets/images/no-image.png?Mobile=1&Source=%2F%5Flayouts%2Fmobile%2Fdispform%2Easpx%3FList%3D78b536db%252De7c7%252D45d9%252Da661%252Ddb2a2aa2fbaf%26View%3D6efc759a%252D0646%252D433c%252Dab6e%252D2f027ffe0799%26RootFolder%3D%252Fwebsites%252Faaru2%252Fwp%252Dcontent%252Fplugins%252Flearnpress%252Fassets%252Fimages%26ID%3D4786%26CurrentPage%3D1"
                 }
                 className="newsImage"
@@ -19,7 +19,7 @@ const NewsCard = ({ newsItem }) => {
                     <span className="title">{newsItem.title}</span>
                     <br />
                     <span className="author">
-                        <a href={newsItem.url} target="__blank">
+                        <a href={newsItem.sourceUrl} target="__blank">
                             <b>Techshort </b>
                         </a>{" "}
                         <span className="muted">
