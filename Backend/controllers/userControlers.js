20fce907cc0aca492610814f781e34ad33d03321
yugg98 <70101980+yugg98@users.noreply.github.com>
2021-12-05 17:59:08 +0530

    server

--- Backend/controllers/userControlers.js
+++ Backend/controllers/userControlers.js
@@ -18,7 +18,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     password,
     avatar: {
       public_id: '987724764823',
-      url: 'https://www.pngplay.com/wp-content/uploads/12/User-Avatar-Profile-PNG-Pic-Clip-Art-Background.png',
+      url: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
     banner: {
       public_id: '02408482034',
